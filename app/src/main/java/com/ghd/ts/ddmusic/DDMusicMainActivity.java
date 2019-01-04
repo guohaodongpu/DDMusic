26c2a9a0a3592b893e67db63fb94d025e766eb97
GuoHaodong <guohd0102@thundersoft.com>
2019-01-04 10:02:15 +0800

    project demo

---
+++ app/src/main/java/com/ghd/ts/ddmusic/DDMusicMainActivity.java
@@ -0,0 +1,13 @@
+package com.ghd.ts.ddmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DDMusicMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ddmusic_main);
+    }
+}
