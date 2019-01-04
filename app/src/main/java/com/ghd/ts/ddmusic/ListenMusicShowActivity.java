a6b343da8053d494b83bcf219e86ba5311101c94
GuoHaodong <guohd0102@thundersoft.com>
2019-01-04 16:18:29 +0800

    ddmusic_01_04

--- app/src/main/java/com/ghd/ts/ddmusic/ListenMusicShowActivity.java
+++ app/src/main/java/com/ghd/ts/ddmusic/ListenMusicShowActivity.java
@@ -1,30 +1,26 @@
 package com.ghd.ts.ddmusic;
 
-import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class DDMusicMainActivity extends AppCompatActivity {
+public class ListenMusicShowActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ddmusic_main);
+        setContentView(R.layout.activity_listen_music_show);
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null){
             actionBar.hide();
         }
-
     }
-
-
-
-    public void listenMusic_show(View v){
-
-        Intent intent=new Intent(DDMusicMainActivity.this,ListenMusicShowActivity.class);
-        startActivity(intent);
+    public void backLast(View v){
+        finish();
     }
 
 }
